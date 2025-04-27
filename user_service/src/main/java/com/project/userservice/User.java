b8101e3e2b4224eadb19e403fe830b7513711ee3
Karimkhan Kikhmetov <kingsleycomangoat@gmail.com>
2025-04-28 02:32:34 +0500

    created user-service| database:h2

---
+++ user_service/src/main/java/com/project/userservice/User.java
@@ -0,0 +1,22 @@
+package com.project.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private Long companyId;
+}
---
+++ user_service/src/main/java/com/project/userservice/UserController.java
@@ -0,0 +1,4 @@
+package com.project.userservice;
+
+public class UserController {
+}
